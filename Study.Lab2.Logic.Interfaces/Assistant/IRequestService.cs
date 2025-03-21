5c5186907c0d6d71a2c96d5dd4d3ecec4b828c08
aggink <aggink@mail.ru>
2025-03-21 20:06:48 +0300

    [Feature] Подготовлен шаблон и пример выполнения Л/Р № 2

---
+++ Study.Lab2.Logic.Interfaces/Assistant/IRequestService.cs
@@ -0,0 +1,19 @@
+﻿namespace Study.Lab2.Logic.Interfaces.Assistant
+{
+    public interface IRequestService
+    {
+        /// <summary>
+        /// Отправить запрос к сервису
+        /// </summary>
+        /// <param name="url">Веб-адрес</param>
+        /// <returns>Ответ от сервиса</returns>
+        string FetchData(string url);
+
+        /// <summary>
+        /// Отправить запрос к сервису
+        /// </summary>
+        /// <param name="url">Веб-адрес</param>
+        /// <returns>Ответ от сервиса</returns>
+        Task<string> FetchDataAsync(string url);
+    }
+}
---
+++ Study.Lab2.Logic/Assistant/RequestService.cs
@@ -0,0 +1,35 @@
+﻿using Study.Lab2.Logic.Interfaces.Assistant;
+
+namespace Study.Lab2.Logic.Assistant
+{
+    public class RequestService : IRequestService
+    {
+        public string FetchData(string url)
+        {
+            using var httpClient = new HttpClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            var response = httpClient.Send(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Ошибка: {response.StatusCode} - {response.ReasonPhrase}");
+            }
+
+            return response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task<string> FetchDataAsync(string url)
+        {
+            using var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Ошибка: {response.StatusCode} - {response.ReasonPhrase}");
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
