17892e49e90bfc0d0fb7020fd8fad38badc6a0d4
student <student@513-10>
2025-03-27 10:49:17 +0300

    [Feature] Добавлен шаблон для Л/Р №1 Задания 1

--- Study.Lab1/Program.cs
+++ Study.Lab1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Study.Lab1.Logic.Assistant;
 using Study.Lab1.Logic.Interfaces;
+using Study.Lab1.Logic.katty;
 using Study.Lab1.Logic.xynthh;
 
 public static class Program
@@ -53,6 +54,8 @@ public static class Program
                 return new AssistantService();
             case ("idb-23-02", 15):
                 return new XynthhService();
+            case ("idb-23-02", 17):
+                return new kattyService();
             default:
                 throw new NotSupportedException();
         }
