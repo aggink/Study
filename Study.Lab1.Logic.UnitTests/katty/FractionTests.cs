a0ee8ef2ac3892b7661528618f43be5f8e8701b8
student <student@513-10>
2025-03-27 10:51:34 +0300

    Добавлен второй тест

--- Study.Lab1.Logic.UnitTests/katty/FractionTests.cs
+++ Study.Lab1.Logic.UnitTests/katty/FractionTests.cs
@@ -19,5 +19,15 @@ namespace Study.Lab1.Logic.UnitTests.katty
 
             Assert.That(text, Is.EqualTo("1/1"));
         }
+
+        [Test]
+        public void ToStringTest2()
+        {
+            var fraction = new Fraction(1, 1);
+
+            var text = fraction.ToString();
+
+            Assert.That(text, Is.EqualTo("1/1"));
+        }
     }
 }
