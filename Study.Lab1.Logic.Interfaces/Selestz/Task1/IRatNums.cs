5e782b48bfa004b855b61f3dd68a3f5cd2f7fd32
Selestz <daniel_2011@mail.ru>
2025-04-02 01:24:54 +0300

    Reapply "Добавлен интерфейс IRatNums и класс RationalNumber"
    This reverts commit 225160d1d3a7b59992eab52585101bae8043ddb3.

---
+++ Study.Lab1.Logic.Interfaces/Selestz/Task1/IRatNums.cs
@@ -0,0 +1,9 @@
+﻿namespace Study.Lab1.Logic.Interfaces.Selestz.Task1
+{
+    public interface IRatNums
+    {
+        public int Numerator { get; }
+
+        public int Denominator { get; }
+    }
+}
