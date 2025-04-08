8c1acbedb48a5298f318cd0f990446ed46b26cff
Alexandr <73179222+aggink@users.noreply.github.com>
2025-04-08 07:22:21 +0300

    [Feature] Пример работы с xlsx-файлом используя установленный Microsoft Excel (#90)
    * [Feature] Пример работы с exel-файлом используя установленный Microsoft Excel
    
    * Update CHANGELOG with latest PR commit
    
    * [Fix] Правка
    
    * [Feature] Добавлено расширение
    
    * [Feature] Добавлен обработчик полученных данных

---
+++ CoreLib.Utilities.Interfaces/Services/IExcelDataProcessorService.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreLib.Utilities.Interfaces.Services
+{
+    public interface IExcelDataProcessorService
+    {
+        /// <summary>
+        /// Загружает данные из Excel-файла, очищает значения в колонке 4 от лишних пробелов, 
+        /// удаляет дубликаты и сохраняет уникальные значения в новый текстовый файл
+        /// </summary>
+        /// <param name="inputFilePath">Путь к исходному Excel-файлу, из которого будут извлечены данные</param>
+        /// <param name="outputFilePath">Путь к текстовому файлу, в который будут записаны уникальные данные</param>
+        void ExportUniqueData(string inputFilePath, string outputFilePath);
+    }
+}
