07a54e1da32f3072cccadb26ffcaca2a23949882
Cherryy <darinasepeleva@gmail.com>
2025-04-09 02:25:35 +0300

    реализация логики рациональных чисел и форматы даты и времени

---
+++ Study.Lab1.Logic.UnitTests/Cherryy/Task2/IFormatDate.cs
@@ -0,0 +1,8 @@
+﻿namespace Study.Lab1.Logic.Interfaces.Cherryy.Task2
+
+{
+    public interface IDateTimeFormatter
+    {
+        string Format(DateTime dateTime);
+    }
+}
---
+++ Study.Lab1.Logic/Cherryy/Task2/EuropeanDateFormatter.cs
@@ -0,0 +1,13 @@
+﻿using Study.Lab1.Logic.Interfaces.Cherryy.Task2;
+using System.Globalization;
+
+namespace Study.Lab1.Logic.Cherryy.Task2
+{
+    public class EuropeanDateTimeFormatter : IDateTimeFormatter
+    {
+        public string Format(DateTime dateTime)
+        {
+            return dateTime.ToString("dd.MM.yyyy HH:mm", CultureInfo.InvariantCulture);
+        }
+    }
+}
