3bcc8ed3cac1d2861d32a4d3a9805952b10f592e
aggink <aggink@mail.ru>
2025-04-08 05:39:16 +0300

    [Feature] Пример работы с exel-файлом используя установленный Microsoft Excel

---
+++ CoreLib.Utilities.Interfaces/Services/IWorksheetReaderService.cs
@@ -0,0 +1,20 @@
+﻿using CoreLib.Utilities.Interfaces.DtoModels.Services;
+
+namespace CoreLib.Utilities.Interfaces.Services
+{
+    public interface IWorksheetReaderService
+    {
+        /// <summary>
+        /// Получить таблицу из файла Excel
+        /// </summary>
+        /// <param name="filePath">Путь к файлу</param>
+        /// <returns>Таблица из файла</returns>
+        /// <remarks>
+        /// Работает только на Windows. Требуется установленный Microsoft Excel.
+        /// </remarks>
+        /// <example>
+        /// Структура таблицы: Column1 | Column2 | Column3 | Column4 | Column5. Первая строка заголовки.
+        /// </example>
+        List<WorksheetDto> LoadFromExcel(string filePath);
+    }
+}
