77129c025175323fa3a9e53cce8f47b2d825b8be
Cherryy <darinasepeleva@gmail.com>
2025-04-09 01:55:38 +0300

    Форматы даты и времени

---
+++ Study.Lab1.Logic/Cherryy/Task2/EuropeanDateFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.Lab1.Logic.Cherryy.Task2
+{
+    class EuropeanDateFormatter
+    {
+    }
+}
