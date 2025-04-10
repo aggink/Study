df97fbfd4fa4294d0eaa4db485d92685cadd46a5
kinkiss1 <gleb445@gmail.com>
2025-04-10 12:27:16 +0300

    Были добавлены два класса и декораторы в них

---
+++ Study.Lab1.Logic/kinkiss1/Task2/AmericanDateFormat.cs
@@ -0,0 +1,64 @@
+﻿using Study.Lab1.Logic.Interfaces.kinkiss1.task2;
+using System.Globalization;
+using System.Text;
+
+namespace Study.Lab1.Logic.kinkiss1.Task2
+{
+
+    public class AmericanDateFormat : IFormatDate
+    {
+        private readonly CultureInfo _culture = new("en-US");
+
+        public string FormatDateTime()
+        {
+            var us = new StringBuilder();
+            us.Append(DateTime.Now.ToString("d", _culture));
+            return us.ToString();
+        }
+    }
+
+    public class AmericanDateFormatDecorator1 : IFormatDate
+    {
+        private readonly AmericanDateFormat _americanDateFormat;
+
+        public AmericanDateFormatDecorator1(AmericanDateFormat format)
+        {
+            _americanDateFormat = format;
+        }
+
+        public string FormatDateTime()
+        {
+            return $"American date format: {_americanDateFormat.FormatDateTime()}";
+        }
+    }
+
+    public class AmericanDateFormatDecorator2 : IFormatDate
+    {
+        private readonly IFormatDate _formatDate;
+
+        public AmericanDateFormatDecorator2(IFormatDate formatDate)
+        {
+            _formatDate = formatDate;
+        }
+
+        public string FormatDateTime()
+        {
+            return $"[Decorated] {_formatDate.FormatDateTime()}";
+        }
+    }
+
+    public class AmericanDateFormatDecorator3 : IFormatDate
+    {
+        private readonly IFormatDate _formatDate;
+
+        public AmericanDateFormatDecorator3(IFormatDate formatDate)
+        {
+            _formatDate = formatDate;
+        }
+
+        public string FormatDateTime()
+        {
+            return $"*** {_formatDate.FormatDateTime()} ***";
+        }
+    }
+}
---
+++ Study.Lab1.Logic/kinkiss1/Task2/EuripeanDateFormat.cs
@@ -0,0 +1,63 @@
+﻿using Study.Lab1.Logic.Interfaces.kinkiss1.task2;
+using System.Globalization;
+using System.Text;
+
+namespace Study.Lab1.Logic.kinkiss1.Task2
+{
+    public class EurepeanDateFormat : IFormatDate
+    {
+        private readonly CultureInfo _culture = new("fr-FR");
+
+        public string FormatDateTime()
+        {
+            var fr = new StringBuilder();
+            fr.Append(DateTime.Now.ToString("d", _culture));
+            return fr.ToString();
+        }
+    }
+
+
+    public class EurepeanDateFormatDecorator1 : IFormatDate
+    {
+        private readonly EurepeanDateFormat _europeanDateFormat;
+
+        public EurepeanDateFormatDecorator1(EurepeanDateFormat format)
+        {
+            _europeanDateFormat = format;
+        }
+
+        public string FormatDateTime()
+        {
+            return $"European date format: {_europeanDateFormat.FormatDateTime()}";
+        }
+    }
+    public class EuropeanDateFormatDecorator2 : IFormatDate
+    {
+        private readonly IFormatDate _formatDate;
+
+        public EuropeanDateFormatDecorator2(IFormatDate formatDate)
+        {
+            _formatDate = formatDate;
+        }
+
+        public string FormatDateTime()
+        {
+            return $"[Decorated] {_formatDate.FormatDateTime()}";
+        }
+    }
+
+    public class EuropeanDateFormatDecorator3 : IFormatDate
+    {
+        private readonly IFormatDate _formatDate;
+
+        public EuropeanDateFormatDecorator3(IFormatDate formatDate)
+        {
+            _formatDate = formatDate;
+        }
+
+        public string FormatDateTime()
+        {
+            return $"*** {_formatDate.FormatDateTime()} ***";
+        }
+    }
+}
