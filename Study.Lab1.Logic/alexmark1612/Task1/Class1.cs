0de05d8e59b445706a967167d16ab89982519af6
student <student@513-01>
2025-04-16 11:44:37 +0300

    [Feature] Промежуточный результат

--- Study.Lab1.Logic/alexmark1612/Task1/Class1.cs
+++ Study.Lab1.Logic/alexmark1612/Task1/Class1.cs
@@ -1,12 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Study.Lab1.Logic.alexmark1612.Task1
+﻿namespace Study.Lab1.Logic.alexmark1612.Task1
 {
-    internal class Class1
+    public class RationalNumbers
     {
+        // Поиск НОД
+        private static int Nod(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (b != 0)
+            {
+                var temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        public int Numerator { get; }
+        public int Denominator { get; }
+        // Конструктор для инициализации переменных
+        public RationalNumbers(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new DivideByZeroException("Знаменатель не может быть равен нулю");
+            if (denominator < 0)
+            {
+                numerator = -numerator;
+                denominator = -denominator;
+            }
+            // Сокращение дроби
+            var gcd = Math.Abs(Nod(numerator, denominator));
+            Numerator = numerator / gcd;
+            Denominator = denominator / gcd;
+        }
+        // Функция для вывода дроби
+        public override string ToString()
+        {
+            if (Denominator != 1)
+            {
+                return $"{Numerator}/{Denominator}";
+            }
+            return $"{Numerator}";
+        }
+        // Перегриузка оператора +
+        public static RationalNumbers operator +(RationalNumbers x, RationalNumbers y)
+        {
+            var a = x.Numerator * y.Denominator + y.Numerator * x.Denominator;
+            var b = x.Denominator * y.Denominator;
+            return new RationalNumbers(a, b);
+        }
+        // Перегриузка оператора -
+        public static RationalNumbers operator -(RationalNumbers x, RationalNumbers y)
+        {
+            var a = x.Numerator * y.Denominator - y.Numerator * x.Denominator;
+            var b = x.Denominator * y.Denominator;
+            return new RationalNumbers(a, b);
+        }
+        // Перегриузка оператора *
+        public static RationalNumbers operator *(RationalNumbers x, RationalNumbers y)
+        {
+            var a = x.Numerator * y.Numerator;
+            var b = x.Denominator * y.Denominator;
+            return new RationalNumbers(a, b);
+        }
+        // Перегриузка оператора /
+        public static RationalNumbers operator /(RationalNumbers x, RationalNumbers y)
+        {
+            var a = x.Numerator * y.Denominator;
+            var b = x.Denominator * y.Numerator;
+            return new RationalNumbers(a, b);
+        }
+        // Перегрузка унарного минуса
+        public static RationalNumbers operator -(RationalNumbers a)
+        {
+            return new RationalNumbers(-a.Numerator, a.Denominator);
+        }
     }
 }
--- Study.Lab1.Logic/alexmark1612/alexmark1612Service.cs
+++ Study.Lab1.Logic/alexmark1612/alexmark1612Service.cs
@@ -1,4 +1,5 @@
-﻿using Study.Lab1.Logic.Interfaces;
+﻿using Study.Lab1.Logic.alexmark1612.Task1;
+using Study.Lab1.Logic.Interfaces;
 
 namespace Study.Lab1.Logic.alexmark1612
 {
@@ -6,8 +7,34 @@ namespace Study.Lab1.Logic.alexmark1612
     {
         public void RunTask1()
         {
-            //
-            throw new NotImplementedException();
+            var a = new RationalNumbers(1, 2);
+            Console.Write($"{a.ToString()}\n");
+            var b = new RationalNumbers(1, 3);
+            Console.Write($"{b.ToString()}\n");
+            var sum = a + b;
+            Console.Write($"{sum.ToString()}\n");
+            var raz = a - b;
+            Console.Write($"{raz.ToString()}\n");
+            var raz2 = b - a;
+            Console.Write($"{raz2.ToString()}\n");
+            var ymn = a * b;
+            Console.Write($"{ymn.ToString()}\n");
+            var del = a / b;
+            Console.Write($"{del.ToString()}\n");
+            var del2 = b / a;
+            Console.Write($"{del2.ToString()}\n");
+            var antia = -a;
+            Console.Write($"{antia.ToString()}\n");
+            var c = new RationalNumbers(2, 4);
+            Console.Write($"{c.ToString()}\n");
+            var d = new RationalNumbers(2, 1);
+            Console.Write($"{d.ToString()}\n");
+            var e = new RationalNumbers(4, 2);
+            Console.Write($"{e.ToString()}\n");
+            var f = new RationalNumbers(7, -2);
+            Console.Write($"{f.ToString()}\n");
+            var g = new RationalNumbers(7, 0);
+            Console.Write($"{g.ToString()}\n");
         }
 
         public void RunTask2()
