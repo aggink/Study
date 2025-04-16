408082d0869660aee6c98286339bfabd11c9b36e
student <student@513-01>
2025-04-16 10:05:53 +0300

    [Feature] Создан шаблон

---
+++ Study.Lab1.Logic/alexmark1612/Task1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.Lab1.Logic.alexmark1612.Task1
+{
+    internal class Class1
+    {
+    }
+}
