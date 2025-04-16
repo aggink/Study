c3d66fa4f21f28a83401209000df5df232675aa5
Student <Student@513-06>
2025-04-16 09:55:47 +0300

    [Feature] Добавлена ЛР №1 Задание 1

---
+++ Study.Lab1.Logic/Pro100futa/Pro100futaService.cs
@@ -0,0 +1,22 @@
+﻿using Study.Lab1.Logic.Interfaces;
+
+namespace Study.Lab1.Logic.Pro100futa
+{
+    public class Pro100futaService : IRunService
+    {
+        public void RunTask1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunTask2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunTask3()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
