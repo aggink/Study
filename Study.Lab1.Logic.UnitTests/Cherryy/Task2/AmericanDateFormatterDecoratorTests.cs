e7dbfa51d022e0b1ff3e54a2ffea1100c98d9b10
che-rry-y <darinasepeleva@gmail.com>
2025-04-21 01:03:30 +0300

    [Feature] Добавлены папки, объекты, классы для реализации задач и изменены ошибки (#95)
    * Добавлены папки, объекты, классы для реализации задач и изменены ошибки
    
    * Update CHANGELOG with latest PR commit
    
    * Update CHANGELOG with latest PR commit
    
    * [Feature] Добавлен декоратор
    
    * Update CHANGELOG with latest PR commit
    
    * [Fix] Правка

---
+++ Study.Lab1.Logic.UnitTests/Cherryy/Task2/AmericanDateFormatterDecoratorTests.cs
@@ -0,0 +1,21 @@
+﻿using Study.Lab1.Logic.Cherryy.Task2;
+using Study.Lab1.Logic.Interfaces.Cherryy.Task2;
+
+namespace Study.Lab1.Logic.UnitTests.Cherryy.Task2
+{
+    [TestFixture]
+    public class AmericanDateFormatterDecoratorTests
+    {
+        [Test]
+        public void AmericanDecorator()
+        {
+            var date = new DateTime(2025, 10, 10, 10, 10, 10);
+
+            var formatter = new AmericanDateTimeFormatter();
+            var decorator = new AmericanDateFormatterDecorator(formatter);
+            var result = decorator.FormatDateTime(date);
+
+            Assert.That(result, Is.EqualTo("American; 10/10/2025 10:10:10 AM"));
+        }
+    }
+}
