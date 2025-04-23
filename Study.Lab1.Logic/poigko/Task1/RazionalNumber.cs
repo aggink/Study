f7c76bac93e99871baeaee0815ae5af1296f32b3
poigko <polina3299@gmail.com>
2025-04-23 10:12:04 +0300

    [Fix] Удаленв неиспольземые using

--- Study.Lab1.Logic/poigko/Task1/RazionalNumber.cs
+++ Study.Lab1.Logic/poigko/Task1/RazionalNumber.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Study.Lab1.Logic.poigko.Task1
+﻿namespace Study.Lab1.Logic.poigko.Task1
 {
     internal class RazionalNumber
     {
--- Study.Lab1.Logic/poigko/poigkoService.cs
+++ Study.Lab1.Logic/poigko/poigkoService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Study.Lab1.Logic.Interfaces;
+﻿using Study.Lab1.Logic.Interfaces;
 
 namespace Study.Lab1.Logic.poigko
 {
