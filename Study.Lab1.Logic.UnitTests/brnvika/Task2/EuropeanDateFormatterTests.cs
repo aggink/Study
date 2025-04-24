13027695fa3b0d5b9ee1edd2e82b28f1998d885b
brnvika <74546783+brnvika@users.noreply.github.com>
2025-04-24 11:51:27 +0300

    Добавлено 2 задание 1 л/р (#110)
    * Добавлено 2 задание 1 л/р
    
    * Update CHANGELOG with latest PR commit
    
    * [Fix] Доработки
    
    * [Fix] Готовое
    
    * [Fix] Откат

---
+++ Study.Lab1.Logic.UnitTests/brnvika/Task2/EuropeanDateFormatterTests.cs
@@ -0,0 +1,110 @@
+﻿using Study.Lab1.Logic.brnvika.Task2;
+using Study.Lab1.Logic.Interfaces.brnvika.Task2;
+
+namespace Study.Lab1.Logic.UnitTests.brnvika.Task2;
+
+[TestFixture]
+public class EuropeanDateFormatterTests
+{
+    [Test]
+    public void FormatDate_ShouldReturnCorrectDateFormat()
+    {
+        // Arrange
+        var formatter = new EuropeanDateFormatter();
+        var testDate = new DateTime(2025, 3, 20);
+
+        // Act
+        var result = formatter.FormatDate(testDate);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("20.03.2025"));
+    }
+
+    [Test]
+    public void FormatDate_ShouldReturnCorrectDateFormat2()
+    {
+        // Arrange
+        var formatter = new EuropeanDateFormatter();
+        var testDate = new DateTime(2025, 3, 21);
+
+        // Act
+        var result = formatter.FormatDate(testDate);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("21.03.2025"));
+    }
+
+    [Test]
+    public void FormatTime_ShouldReturnCorrectTimeFormat()
+    {
+        // Arrange
+        var formatter = new EuropeanDateFormatter();
+        var testTime = new DateTime(2025, 3, 20, 14, 30, 45);
+
+        // Act
+        var result = formatter.FormatTime(testTime);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("14:30:45"));
+    }
+
+    [Test]
+    public void FormatDate_ShouldReturnCorrectFormatAddSeparator()
+    {
+        // Arrange
+        IDateFormatter formatter = new EuropeanDateFormatter();
+        var testDate = new DateTime(2025, 4, 22, 14, 30, 45);
+        formatter = new AddNewSeparator(formatter, '*');
+
+        // Act
+        var result = formatter.FormatDate(testDate);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("22*04*2025"));
+    }
+
+    [Test]
+    public void FormatTime_ShouldReturnCorrectFormatAddSeparator()
+    {
+        // Arrange
+        IDateFormatter formatter = new EuropeanDateFormatter();
+        var testTime = new DateTime(2025, 4, 22, 14, 30, 45);
+        formatter = new AddNewSeparator(formatter, '*');
+
+        // Act
+        var result = formatter.FormatTime(testTime);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("14*30*45"));
+    }
+
+    [Test]
+    public void FormatDate_ShouldReturnCorrectFormatIsolateLine()
+    {
+        // Arrange
+        IDateFormatter formatter = new EuropeanDateFormatter();
+        var testDate = new DateTime(2025, 4, 22, 14, 30, 45);
+        formatter = new IsolateLine(formatter, "##");
+
+        // Act
+        var result = formatter.FormatDate(testDate);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("##22.04.2025##"));
+    }
+
+    [Test]
+    public void FormatTime_ShouldReturnCorrectFormatIsolateLine()
+    {
+        // Arrange
+        IDateFormatter formatter = new EuropeanDateFormatter();
+        var testTime = new DateTime(2025, 4, 22, 14, 30, 45);
+        formatter = new IsolateLine(formatter, "##");
+
+        // Act
+        var result = formatter.FormatTime(testTime);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("##14:30:45##"));
+    }
+}
