c8fe20c903991b8c519a612e58d903628b967b7d
brnvika <74546783+brnvika@users.noreply.github.com>
2025-04-24 11:23:51 +0300

    [Feature] Добавлено первое задание (#96)
    * [Feature] Добавлена реализация л/р зад 1
    
    * [Feature] что-то
    
    * [Feature] Что-то
    
    * Update CHANGELOG with latest PR commit
    
    * Update CHANGELOG with latest PR commit
    
    * [Fix] Откат
    
    * Update CHANGELOG with latest PR commit
    
    * [Fix] Откат

--- Study.Lab1/Program.cs
+++ Study.Lab1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Study.Lab1.Logic.Assistant;
+using Study.Lab1.Logic.brnvika;
+using Study.Lab1.Logic.Cherryy;
 using Study.Lab1.Logic.eldarovskiy;
 using Study.Lab1.Logic.Interfaces;
 using Study.Lab1.Logic.Jki749;
@@ -8,14 +10,13 @@ using Study.Lab1.Logic.lsokol14l;
 using Study.Lab1.Logic.PresvyatoyKabachok;
 using Study.Lab1.Logic.Selestz;
 using Study.Lab1.Logic.xynthh;
-using Study.Lab1.Logic.Cherryy;
 
 public static class Program
 {
     /// <summary>
     /// Номер выполняемой задачи
     /// </summary>
-    private const int RUN_TASK_NUMBER = 2;
+    private const int RUN_TASK_NUMBER = 1;
 
     /// <summary>
     /// Название группы
@@ -77,6 +78,8 @@ public static class Program
                 return new EldarovskiyService();
             case ("idb-23-03", 22):
                 return new Jki749Service();
+            case ("idb-23-02", 4):
+                return new brnvikaService();
             default:
                 throw new NotSupportedException();
         }
