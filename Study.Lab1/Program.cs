9508f502302a6fbe730e52b60d7a898654c30c59
brnvika <vika-05.brnv@mail.ru>
2025-04-24 11:15:19 +0300

    [Fix] Откат к началу

--- Study.Lab1/Program.cs
+++ Study.Lab1/Program.cs
@@ -13,17 +13,17 @@ public static class Program
     /// <summary>
     /// Номер выполняемой задачи
     /// </summary>
-    private const int RUN_TASK_NUMBER = 3;
+    private const int RUN_TASK_NUMBER = 1;
 
     /// <summary>
     /// Название группы
     /// </summary>
-    private const string GROUP_NAME = "idb-23-02";
+    private const string GROUP_NAME = "assistant";
 
     /// <summary>
     /// Порядковый номер
     /// </summary>
-    private const int PERSON_NUMBER = 4;
+    private const int PERSON_NUMBER = 1;
 
     public static void Main()
     {
