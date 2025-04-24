3dbc7bf60ac441439b00b78d5f21948dcb21937c
kinkiss1 <gleb445@gmail.com>
2025-04-24 20:35:34 +0300

    Сделан Task2.

---
+++ Study.Lab1.Logic/kinkiss1/Task2/EuropeanDateFormat.cs
@@ -0,0 +1,18 @@
+﻿using Study.Lab1.Logic.Interfaces.kinkiss1.task2;
+using System.Globalization;
+using System.Text;
+
+namespace Study.Lab1.Logic.kinkiss1.Task2
+{
+    public class EuropeanDateFormat : IFormatDate
+    {
+        private readonly CultureInfo _culture = new("fr-FR");
+
+        public string FormatDateTime()
+        {
+            var fr = new StringBuilder();
+            fr.Append(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss", _culture)); // Добавлено время
+            return fr.ToString();
+        }
+    }
+}
