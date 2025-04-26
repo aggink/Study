496b5136c25f2700af779d5a1f71e53ff62087a1
Alex-1000 <neijr@proton.me>
2025-04-26 20:27:00 +0300

    [Fix] Добавлена задача в Service

--- Study.Lab1.Logic/neijrr/neijrrService.cs
+++ Study.Lab1.Logic/neijrr/neijrrService.cs
@@ -1,4 +1,7 @@
+using Study.Lab1.Logic.Assistant.Task2;
 using Study.Lab1.Logic.Interfaces;
+using Study.Lab1.Logic.kinkiss1.Task2;
+using Study.Lab1.Logic.neijrr.Task2;
 
 namespace Study.Lab1.Logic.neijrr
 {
@@ -23,12 +26,31 @@ namespace Study.Lab1.Logic.neijrr
             var a = new Fraction(2, 3);
             var b = new Fraction(1, 6);
             var c = a + b;
+
             Console.WriteLine($"{a} + {b} = {c}");
         }
 
         public void RunTask2()
         {
-            throw new NotImplementedException();
+            var euFormatter = new EuropeanDateTimeFormatter();
+            var euDateFormatter = new PrefixDecorator(euFormatter, "Сегодня ");
+            var euTimeFormatter = new SuffixDecorator(new PrefixDecorator(euFormatter, "Время: ["), "]");
+
+            var amFormatter = new AmericanDateTimeFormatter();
+            var amDateFormatter = new PrefixDecorator(amFormatter, "Сегодня ");
+            var amTimeFormatter = new SuffixDecorator(new PrefixDecorator(amFormatter, "Время: ["), "]");
+
+            Console.WriteLine("Европейский формат:");
+            Console.WriteLine(euDateFormatter.Date());
+            Console.WriteLine(euTimeFormatter.Date());
+
+            Console.WriteLine("Американский формат:");
+            Console.WriteLine(amDateFormatter.Date());
+            Console.WriteLine(amTimeFormatter.Date());
+
+            Console.WriteLine("Unix epoch:");
+            Console.WriteLine($"Европейский формат: {euFormatter.DateTime(DateTime.UnixEpoch)}");
+            Console.WriteLine($"Американский формат: {amFormatter.DateTime(DateTime.UnixEpoch)}");
         }
 
         public void RunTask3()
