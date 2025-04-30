8f519e0053070e23519689ede1433d95fbcfa36e
Александр Уколов <aggink@mail.ru>
2025-04-30 10:07:01 +0300

    [Fix] Стиль

--- Study.Lab1/Program.cs
+++ Study.Lab1/Program.cs
@@ -1,4 +1,4 @@
-using Study.Lab1.Logic.Assistant;
+﻿using Study.Lab1.Logic.Assistant;
 using Study.Lab1.Logic.brnvika;
 using Study.Lab1.Logic.Cherryy;
 using Study.Lab1.Logic.eldarovskiy;
@@ -15,79 +15,79 @@ using Study.Lab1.Logic.xynthh;
 
 public static class Program
 {
-	/// <summary>
-	/// Номер выполняемой задачи
-	/// </summary>
-	private const int RUN_TASK_NUMBER = 1;
+    /// <summary>
+    /// Номер выполняемой задачи
+    /// </summary>
+    private const int RUN_TASK_NUMBER = 1;
 
-	/// <summary>
-	/// Название группы
-	/// </summary>
-	private const string GROUP_NAME = "assistant";
+    /// <summary>
+    /// Название группы
+    /// </summary>
+    private const string GROUP_NAME = "assistant";
 
-	/// <summary>
-	/// Порядковый номер
-	/// </summary>
-	private const int PERSON_NUMBER = 1;
+    /// <summary>
+    /// Порядковый номер
+    /// </summary>
+    private const int PERSON_NUMBER = 1;
 
-	public static void Main()
-	{
-		var service = GetRunLabService(GROUP_NAME, PERSON_NUMBER);
+    public static void Main()
+    {
+        var service = GetRunLabService(GROUP_NAME, PERSON_NUMBER);
 
-		switch (RUN_TASK_NUMBER)
-		{
-			case 1:
-				service.RunTask1();
-				break;
-			case 2:
-				service.RunTask2();
-				break;
-			case 3:
-				service.RunTask3();
-				break;
-			default:
-				throw new NotSupportedException();
-		}
-	}
+        switch (RUN_TASK_NUMBER)
+        {
+            case 1:
+                service.RunTask1();
+                break;
+            case 2:
+                service.RunTask2();
+                break;
+            case 3:
+                service.RunTask3();
+                break;
+            default:
+                throw new NotSupportedException();
+        }
+    }
 
-	/// <summary>
-	/// Получить сервис для выполнения задач
-	/// </summary>
-	/// <param name="group">Название группы</param>
-	/// <param name="number">Порядковый номер</param>
-	/// <returns>Сервис для выполнения задач</returns>
-	private static IRunService GetRunLabService(string group, int number)
-	{
-		switch (group, number)
-		{
-			case ("assistant", 1):
-				return new AssistantService();
-			case ("idb-23-02", 15):
-				return new XynthhService();
-			case ("idb-23-02", 6):
-				return new Kinkiss1Service();
-			case ("idb-23-02", 17):
-				return new kattyService();
-			case ("idb-23-02", 19):
-				return new lsokol14lService();
-			case ("idb-23-02", 23):
-				return new SelestzService();
-			case ("idb-23-02", 10):
-				return new PresvyatoyKabachokService();
-			case ("idb-23-02", 24):
-				return new CherryyService();
-			case ("idb-23-02", 2):
-				return new EldarovskiyService();
-			case ("idb-23-03", 22):
-				return new Jki749Service();
-			case ("idb-23-03", 6):
-				return new neijrrService();
-			case ("idb-23-02", 4):
-				return new brnvikaService();
-			case ("idb-23-03", 23):
-				return new Pro100futaService();
-			default:
-				throw new NotSupportedException();
-		}
-	}
+    /// <summary>
+    /// Получить сервис для выполнения задач
+    /// </summary>
+    /// <param name="group">Название группы</param>
+    /// <param name="number">Порядковый номер</param>
+    /// <returns>Сервис для выполнения задач</returns>
+    private static IRunService GetRunLabService(string group, int number)
+    {
+        switch (group, number)
+        {
+            case ("assistant", 1):
+                return new AssistantService();
+            case ("idb-23-02", 15):
+                return new XynthhService();
+            case ("idb-23-02", 6):
+                return new Kinkiss1Service();
+            case ("idb-23-02", 17):
+                return new kattyService();
+            case ("idb-23-02", 19):
+                return new lsokol14lService();
+            case ("idb-23-02", 23):
+                return new SelestzService();
+            case ("idb-23-02", 10):
+                return new PresvyatoyKabachokService();
+            case ("idb-23-02", 24):
+                return new CherryyService();
+            case ("idb-23-02", 2):
+                return new EldarovskiyService();
+            case ("idb-23-03", 22):
+                return new Jki749Service();
+            case ("idb-23-03", 6):
+                return new neijrrService();
+            case ("idb-23-02", 4):
+                return new brnvikaService();
+            case ("idb-23-03", 23):
+                return new Pro100futaService();
+            default:
+                throw new NotSupportedException();
+        }
+    }
 }
