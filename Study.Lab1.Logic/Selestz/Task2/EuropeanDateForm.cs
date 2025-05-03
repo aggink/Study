7e8568324750ecf37f777244ce3af6befd377ff9
Selestz <daniel_2011@mail.ru>
2025-05-03 19:57:43 +0300

    Добавлены классы для вывода даты и времени в двух форматах, реализованы декораторы и написаны тесты

---
+++ Study.Lab1.Logic/Selestz/Task2/EuropeanDateForm.cs
@@ -0,0 +1,17 @@
+﻿using Study.Lab1.Logic.Interfaces.Selestz.Task2;
+using System.Globalization;
+using System.Text;
+
+namespace Study.Lab1.Logic.Selestz.Task2;
+
+public class EuropeanDateForm : IDateTimeFormatter
+{
+    private readonly CultureInfo _culture = new("de-DE");
+
+    public string FormatDateTime()
+    {
+        var deutschland = new StringBuilder();
+        deutschland.Append(DateTime.Now.ToString("g", _culture)); // Добавлено время
+        return deutschland.ToString();
+    }
+}
