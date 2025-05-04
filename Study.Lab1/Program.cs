314d7c33de55dccce1955e1d7f7ac011be141658
PresvyatoyKabachok <m9163936843@yandex.ru>
2025-05-04 05:07:55 +0300

    [Feature] Добавлено Задание 2 Лабораторная Работа 1

--- Study.Lab1/Program.cs
+++ Study.Lab1/Program.cs
@@ -2,6 +2,7 @@
 using Study.Lab1.Logic.brnvika;
 using Study.Lab1.Logic.Cherryy;
 using Study.Lab1.Logic.eldarovskiy;
+using Study.Lab1.Logic.gegemonTV;
 using Study.Lab1.Logic.Interfaces;
 using Study.Lab1.Logic.Jki749;
 using Study.Lab1.Logic.katty;
@@ -12,9 +13,6 @@ using Study.Lab1.Logic.PresvyatoyKabachok;
 using Study.Lab1.Logic.Pro100futa;
 using Study.Lab1.Logic.Selestz;
 using Study.Lab1.Logic.xynthh;
-using Study.Lab1.Logic.Cherryy;
-using Study.Lab1.Logic.neijrr;
-using Study.Lab1.Logic.gegemonTV;
 
 
 public static class Program
@@ -22,7 +20,7 @@ public static class Program
     /// <summary>
     /// Номер выполняемой задачи
     /// </summary>
-    private const int RUN_TASK_NUMBER = 1;
+    private const int RUN_TASK_NUMBER = 2;
 
     /// <summary>
     /// Название группы
@@ -32,7 +30,7 @@ public static class Program
     /// <summary>
     /// Порядковый номер
     /// </summary>
-    private const int PERSON_NUMBER = 0;
+    private const int PERSON_NUMBER = 1;
 
     public static void Main()
     {
