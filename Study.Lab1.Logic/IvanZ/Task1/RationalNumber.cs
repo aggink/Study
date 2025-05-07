9f6ac147cd1a5614f7f932f88b36179b7cb606bc
IvanZ-code <114724416+IvanZ-code@users.noreply.github.com>
2025-05-07 16:26:36 +0300

    [Private] обновление кодстайла

--- Study.Lab1.Logic/IvanZ/Task1/RationalNumber.cs
+++ Study.Lab1.Logic/IvanZ/Task1/RationalNumber.cs
@@ -2,94 +2,94 @@ namespace Study.Lab1.Logic.IvanZ.Task1;
 
 public class RationalNumber
 {
-public int Numerator { get; }
-public int Denominator { get; }
+	public int Numerator { get; }
+	public int Denominator { get; }
       
-public RationalNumber(int numerator, int denominator)
-{
-        if (denominator == 0)
-        throw new DivideByZeroException("Denominator can't be zero");
+	public RationalNumber(int numerator, int denominator)
+	{
+        	if (denominator == 0)
+        	throw new DivideByZeroException("Denominator can't be zero");
 
-        if (denominator < 0)
-        {
-        numerator = -numerator;
-        denominator = -denominator;
-        }
+        	if (denominator < 0)
+        	{
+        	numerator = -numerator;
+        	denominator = -denominator;
+        	}
 
-        int nod = NOD(Math.Abs(numerator), Math.Abs(denominator));
-        Numerator = numerator / nod;
-        Denominator = denominator / nod;
-}
+        	int nod = NOD(Math.Abs(numerator), Math.Abs(denominator));
+        	Numerator = numerator / nod;
+        	Denominator = denominator / nod;
+	}
 
-public override string ToString()
-{
-        return Denominator == 1 ? Numerator.ToString() : $"{Numerator}/{Denominator}";
-}
+	public override string ToString()
+	{
+        	return Denominator == 1 ? Numerator.ToString() : $"{Numerator}/{Denominator}";
+	}
 
-public static RationalNumber operator +(RationalNumber a, RationalNumber b)
-{
-        return new RationalNumber(a.Numerator * b.Denominator + b.Numerator * a.Denominator, a.Denominator * b.Denominator);
-}
+	public static RationalNumber operator +(RationalNumber a, RationalNumber b)
+	{
+        	return new RationalNumber(a.Numerator * b.Denominator + b.Numerator * a.Denominator, a.Denominator * b.Denominator);
+	}
       
-public static RationalNumber operator -(RationalNumber a, RationalNumber b)
-{
-        return new RationalNumber(a.Numerator * b.Denominator - b.Numerator * a.Denominator, a.Denominator * b.Denominator);
-}
+	public static RationalNumber operator -(RationalNumber a, RationalNumber b)
+	{
+        	return new RationalNumber(a.Numerator * b.Denominator - b.Numerator * a.Denominator, a.Denominator * b.Denominator);
+	}
       
-public static RationalNumber operator *(RationalNumber a, RationalNumber b)
-{
-        return new RationalNumber(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
-}
+	public static RationalNumber operator *(RationalNumber a, RationalNumber b)
+	{
+        	return new RationalNumber(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
+	}
       
-public static RationalNumber operator /(RationalNumber a, RationalNumber b)
-{
-        return new RationalNumber(a.Numerator * b.Denominator, a.Denominator * b.Numerator);
-}
+	public static RationalNumber operator /(RationalNumber a, RationalNumber b)
+	{
+        	return new RationalNumber(a.Numerator * b.Denominator, a.Denominator * b.Numerator);
+	}
       
-public static RationalNumber operator -(RationalNumber a)
-{
-        return new RationalNumber(-a.Numerator, a.Denominator);
-}
+	public static RationalNumber operator -(RationalNumber a)
+	{
+        	return new RationalNumber(-a.Numerator, a.Denominator);
+	}
       
-public static bool operator ==(RationalNumber a, RationalNumber b)
-{
-        return (a.Numerator == b.Numerator) && (a.Denominator == b.Denominator);
-}
+	public static bool operator ==(RationalNumber a, RationalNumber b)
+	{
+        	return (a.Numerator == b.Numerator) && (a.Denominator == b.Denominator);
+	}
       
-public static bool operator !=(RationalNumber a, RationalNumber b)
-{
-        return !(a == b);
-}
+	public static bool operator !=(RationalNumber a, RationalNumber b)
+	{
+        	return !(a == b);
+	}
       
-public static bool operator <(RationalNumber a, RationalNumber b)
-{
-        return a.Numerator * b.Denominator < b.Numerator * a.Denominator;
-}
+	public static bool operator <(RationalNumber a, RationalNumber b)
+	{
+        	return a.Numerator * b.Denominator < b.Numerator * a.Denominator;
+	}
       
-public static bool operator <=(RationalNumber a, RationalNumber b)
-{
-        return a.Numerator * b.Denominator <= b.Numerator * a.Denominator;
-}
+	public static bool operator <=(RationalNumber a, RationalNumber b)
+	{
+        	return a.Numerator * b.Denominator <= b.Numerator * a.Denominator;
+	}
       
-public static bool operator >(RationalNumber a, RationalNumber b)
-{
-        return !(a <= b);
-}
+	public static bool operator >(RationalNumber a, RationalNumber b)
+	{
+        	return !(a <= b);
+	}
       
-public static bool operator >=(RationalNumber a, RationalNumber  b)
-{
-        return !(a < b);
-}
+	public static bool operator >=(RationalNumber a, RationalNumber  b)
+	{
+       		return !(a < b);
+	}
       
-private static int NOD(int a, int b)
-{
-        while (b != 0)
-        {
-        	int temp = b;
-         	b = a % b;
-                a = temp;
-        }
-        return a;
-}
+	private static int NOD(int a, int b)
+	{
+        	while (b != 0)
+        	{
+        		int temp = b;
+         		b = a % b;
+                	a = temp;
+        	}
+        	return a;
+	}
 	
 }; 
