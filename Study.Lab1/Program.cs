942aa3dffc3ec42c5c5396227bb12e68b27f9de4
TucKaW <kriper.full@gmail.com>
2025-05-11 21:38:24 +0300

    [Fix] Вернул значения program.cs

--- Study.Lab1/Program.cs
+++ Study.Lab1/Program.cs
@@ -32,12 +32,12 @@ public static class Program
     /// <summary>
     /// Название группы
     /// </summary>
-    private const string GROUP_NAME = "idb-23-03";
+    private const string GROUP_NAME = "assistant";
 
     /// <summary>
     /// Порядковый номер
     /// </summary>
-    private const int PERSON_NUMBER = 9;
+    private const int PERSON_NUMBER = 1;
 
     public static void Main()
     {
