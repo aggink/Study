c81b6640637465966672d5a0a77c2a97918d3e58
Александр Уколов <aggink@mail.ru>
2025-05-13 18:25:22 +0300

    [Feature] Добавлен MS SQL провайдер для базы данных

---
+++ Study.Lab3.Storage/Enums/DatabaseProviderType.cs
@@ -0,0 +1,17 @@
+﻿namespace Study.Lab3.Storage.Enums;
+
+/// <summary>
+/// Тип провайдера базы данных
+/// </summary>
+public enum DatabaseProviderType
+{
+    /// <summary>
+    /// PostgreSql
+    /// </summary>
+    PostgreSql,
+
+    /// <summary>
+    /// Microsoft Sql
+    /// </summary>
+    MSSql
+}
---
+++ Study.Lab3.Web/Extensions/ConfigurationExceptions.cs
@@ -0,0 +1,38 @@
+﻿using Study.Lab3.Storage.Enums;
+
+namespace Study.Lab3.Web.Extensions;
+
+public static class ConfigurationExceptions
+{
+    /// <summary>
+    /// Получить провайдер базы данных
+    /// </summary>
+    /// <param name="configuration">Набор свойств конфигурации</param>
+    /// <returns>Провайдер базы данных</returns>
+    public static DatabaseProviderType GetDatabaseProvider(this IConfiguration configuration)
+    {
+        var provider = configuration.GetDatabaseProviderText();
+
+        return provider switch
+        {
+            "PostgreSql" => DatabaseProviderType.PostgreSql,
+            "MSSql" => DatabaseProviderType.MSSql,
+            _ => throw new Exception("Провайдер базы данных не задан")
+        };
+    }
+
+    /// <summary>
+    /// Получить провайдер базы данных
+    /// </summary>
+    /// <param name="configuration">Набор свойств конфигурации</param>
+    /// <returns>Провайдер базы данных</returns>
+    private static string GetDatabaseProviderText(this IConfiguration configuration)
+    {
+        var provider = configuration["DatabaseProvider"];
+
+        if (string.IsNullOrEmpty(provider))
+            throw new Exception("Провайдер базы данных не задан");
+
+        return provider;
+    }
+}
