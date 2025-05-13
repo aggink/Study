a68aa4faa98e862c4f24b3360cfb0e823ecbc5b1
TucKaW <kriper.full@gmail.com>
2025-05-13 23:36:09 +0300

    [Fix] Убрал {}

--- Study.Lab1.Logic/TucKaW/TucKaWService.cs
+++ Study.Lab1.Logic/TucKaW/TucKaWService.cs
@@ -1,6 +1,5 @@
 ﻿using Study.Lab1.Logic.Interfaces;
 using Study.Lab1.Logic.TucKaW.Task1;
-using System.Text;
 
 namespace Study.Lab1.Logic.TucKaW;
 
@@ -8,37 +7,35 @@ public class TucKaWService : IRunService
 {
     public void RunTask1()
     {
-        {
-            var a = new RationalNumber(1, 2);
-            var b = new RationalNumber(3, 4);
-            var c = new RationalNumber(5, 10);
-            var d = new RationalNumber(8, 2);
-            var e = new RationalNumber(-12, -3);
+        var a = new RationalNumber(1, 2);
+        var b = new RationalNumber(3, 4);
+        var c = new RationalNumber(5, 10);
+        var d = new RationalNumber(8, 2);
+        var e = new RationalNumber(-12, -3);
 
-            var sum = a + b; // Используем перегруженный оператор +
-            var difference = a - b; // Используем перегруженный оператор -
-            var product = a * b; // Используем перегруженный оператор *
-            var quotient = a / b; // Используем перегруженный оператор /
-            var areEqual = a == b; // Используем перегруженный оператор ==
+        var sum = a + b; // Используем перегруженный оператор +
+        var difference = a - b; // Используем перегруженный оператор -
+        var product = a * b; // Используем перегруженный оператор *
+        var quotient = a / b; // Используем перегруженный оператор /
+        var areEqual = a == b; // Используем перегруженный оператор ==
 
-            Console.Write($"a: {a}, b: {b}, c:5/10\n" +
-                          $"a + b = {sum}\n" +
-                          $"a - b = {difference}\n" +
-                          $"a * b = {product}\n" +
-                          $"a / b = {quotient}\n" +
-                          $"a == b ? {areEqual}\n" +
-                          $"c: {c}\n" +
-                          $"d: {d}\n" +
-                          $"e: {e}\n" +
-                          $"-e: {-e}\n" +
-                          $"a>b: {a > b}\n" +
-                          $"b<a: {b < a}\n" +
-                          $"c<=b: {c <= b}\n" +
-                          $"d>=b: {d >= b}\n"
-            );
+        Console.Write($"a: {a}, b: {b}, c:5/10\n" +
+                      $"a + b = {sum}\n" +
+                      $"a - b = {difference}\n" +
+                      $"a * b = {product}\n" +
+                      $"a / b = {quotient}\n" +
+                      $"a == b ? {areEqual}\n" +
+                      $"c: {c}\n" +
+                      $"d: {d}\n" +
+                      $"e: {e}\n" +
+                      $"-e: {-e}\n" +
+                      $"a>b: {a > b}\n" +
+                      $"b<a: {b < a}\n" +
+                      $"c<=b: {c <= b}\n" +
+                      $"d>=b: {d >= b}\n"
+        );
 
-            // e.Denominator = 0; Ошибка => только для чтения;
-        }
+        // e.Denominator = 0; Ошибка => только для чтения;
     }
 
     public void RunTask2()
