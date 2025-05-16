902186c8e17a02cc4f402e3c4c5db68cf6bb2e28
kattyap <2817302@mail.ru>
2025-05-16 19:56:10 +0300

    [Feature] Лабораторная 3

---
+++ Study.Lab3.Storage/Models/University/TeacherSubject.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Study.Lab3.Storage.Models.University;
+
+/// <summary>
+/// Связь таблиц учителя - предметы
+/// </summary>
+[Index(nameof(IsnTeacher), nameof(IsnSubject))]
+[PrimaryKey(nameof(IsnTeacher), nameof(IsnSubject))]
+public class TeacherSubject
+{
+    /// <summary>
+    /// Идентификатор учителя
+    /// </summary>
+    [ForeignKey(nameof(Teacher)), Required]
+    public Guid IsnTeacher { get; set; }
+
+    /// <summary>
+    /// Идентификатор предмета
+    /// </summary>
+    [ForeignKey(nameof(Subject)), Required]
+    public Guid IsnSubject { get; set; }
+
+    /// <summary>
+    /// Учитель
+    /// </summary>
+    public virtual Teacher Teacher { get; set; }
+
+    /// <summary>
+    /// Предмет
+    /// </summary>
+    public virtual Subject Subject { get; set; }
+}
---
+++ Study.Lab3.Web/Features/University/TeacherSubjects/DtoModels/TeacherSubjectListItemDto.cs
@@ -0,0 +1,21 @@
+﻿using Study.Lab3.Web.Features.University.Subjects.DtoModels;
+
+namespace Study.Lab3.Web.Features.University.TeacherSubjects.DtoModels;
+
+public sealed record TeacherSubjectListItemDto
+{
+    /// <summary>
+    /// Идентификатор учителя
+    /// </summary>
+    public Guid IsnTeacher { get; init; }
+
+    /// <summary>
+    /// ФИО учителя
+    /// </summary>
+    public string TeacherFullName { get; init; }
+
+    /// <summary>
+    /// Список предметов учителя
+    /// </summary>
+    public SubjectDto[] Subjects { get; init; }
+}
