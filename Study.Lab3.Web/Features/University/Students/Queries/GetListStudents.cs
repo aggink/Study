9793185c91f57643b76210a6c92bf13a44d336f8
Александр Уколов <aggink@mail.ru>
2025-05-22 19:06:11 +0300

    [Fix] Исправлен запрос

--- Study.Lab3.Web/Features/University/Students/Queries/GetListStudents.cs
+++ Study.Lab3.Web/Features/University/Students/Queries/GetListStudents.cs
@@ -27,6 +27,9 @@ public class GetListStudentsQueryHandler : IRequestHandler<GetListStudentsQuery,
     {
         return await _dataContext.Students
             .AsNoTracking()
+            .OrderBy(x => x.SurName)
+                .ThenBy(x => x.Name)
+                    .ThenBy(x => x.PatronymicName)
             .Select(x => new StudentItemDto
             {
                 IsnStudent = x.IsnStudent,
@@ -35,7 +38,6 @@ public class GetListStudentsQueryHandler : IRequestHandler<GetListStudentsQuery,
                 Sex = x.Sex,
                 Age = x.Age
             })
-            .OrderBy(x => x.Fio)
             .ToArrayAsync(cancellationToken);
     }
 }
