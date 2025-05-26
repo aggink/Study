f4cd762586f05bfed71a3213b1d27007dee1fef7
Jki748 <jki749@mail.ru>
2025-05-26 13:39:07 +0300

    [Private] перемещение скобки

--- Study.Lab2.Logic.Interfaces/Jki749/IRequestService.cs
+++ Study.Lab2.Logic.Interfaces/Jki749/IRequestService.cs
@@ -1,8 +1,7 @@
 ﻿namespace Study.Lab2.Logic.Interfaces.Jki749;
 
-public interface IRequestService : IDisposable
-    {
+public interface IRequestService : IDisposable {
         string FetchData(string url);
 
         Task<string> FetchDataAsync(string url, CancellationToken cancellationToken = default);
-    }
+}
