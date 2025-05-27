4b5e936b7c8999b01cd6bb7699b86f01001fa1d8
alexmark1612 <122559605+alexmark1612@users.noreply.github.com>
2025-05-27 18:35:23 +0300

    [Features] Сделал лр 1 задание 1 (#197)
    * [Features] Сделал лр 1 задание 1
    
    * [Feature] внёс изменения в лр 1
    
    * [features] добавил пробелы

--- Study.Lab1/Program.cs
+++ Study.Lab1/Program.cs
@@ -27,6 +27,7 @@ using Study.Lab1.Logic.Taipano;
 using Study.Lab1.Logic.TucKaW;
 using Study.Lab1.Logic.danaky1;
 using Study.Lab1.Logic.yamisakimei;
+using Study.Lab1.Logic.alexmark1612;
 using Study.Lab1.Logic.Dronio1337;
 
 public static class Program
@@ -132,7 +133,9 @@ public static class Program
                 return new danaky1Service();
             case ("idb-23-03", 8):
                 return new yamisakimeiService();
-                case("idb-23-02", 5):
+            case ("idb-23-03", 13):
+                return new alexmark1612Service();
+            case("idb-23-02", 5):
                 return new Dronio1337Service();
             default:
                 throw new NotSupportedException();
