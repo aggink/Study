19876a08e388eae643cf6c78d1f7637b6b750ee3
brnvika <74546783+brnvika@users.noreply.github.com>
2025-05-28 23:47:58 +0300

    [Feature] Добавлено Л/Р №3 (#201)
    * [Feature] Л/Р№3
    
    * Л.Р3
    
    * [Feature] Л/Р №3 SQl
    
    * [FIX] Л/Р №3
    
    * [FIX] Л/Р №3 исправленная
    
    * [FIX] Л/Р №3 Миграции
    
    * [FIX] Удалены миграции
    
    * [FIX] Л.Р3
    
    * [FIX] Л/Р №3 с правками
    
    * [FIX] Миграции исправлены
    
    * [Fix] Исправлена работа контроллера, созданы миграции.
    
    * [Fix] Изменена логика в в некоторых запросах

---
+++ Study.Lab3.Web/Features/Library/Books/DtoModels/BookWithAuthorsItemDto.cs
@@ -0,0 +1,26 @@
+﻿using Study.Lab3.Web.Features.Library.Authors.DtoModels;
+
+namespace Study.Lab3.Web.Features.Library.Books.DtoModels;
+
+public class BookWithAuthorsItemDto
+{
+    /// <summary>
+    /// Идентификатор книги
+    /// </summary>
+    public Guid IsnBook { get; init; }
+
+    /// <summary>
+    /// Название книги
+    /// </summary>
+    public string Title { get; init; }
+
+    /// <summary>
+    /// Год издания
+    /// </summary>
+    public int PublicationYear { get; init; }
+
+    /// <summary>
+    /// Список авторов
+    /// </summary>
+    public AuthorItemDto[] Authors { get; init; }
+}
