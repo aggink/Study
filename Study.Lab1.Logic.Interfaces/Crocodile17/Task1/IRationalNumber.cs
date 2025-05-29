16a8069c39bbcce8f8bc7ec8cc1c56de9142a26e
Crocodile17 <for.vuz.alexr@gmail.com>
2025-05-29 22:03:57 +0300

    [Feature] Добавленная лабораторная 1 задание 1 после замечаний

--- Study.Lab1.Logic.Interfaces/Crocodile17/Task1/IRationalNumber.cs
+++ Study.Lab1.Logic.Interfaces/Crocodile17/Task1/IRationalNumber.cs
@@ -1,9 +1,8 @@
-namespace Study.Lab1.Logic.Interfaces.Crocodile17.Task1
-{
-    public interface IRationalNumber
-    {
-        public long Numerator { get; }
+namespace Study.Lab1.Logic.Interfaces.Crocodile17.Task1;
 
-        public long Denominator { get; }
-    }
+public interface IRationalNumber
+{
+    public long Numerator { get; }
+
+    public long Denominator { get; }
 }
