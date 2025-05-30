7da0893c2370ede1a4e3954f714164f3e31251d5
Alexandr <73179222+aggink@users.noreply.github.com>
2025-05-30 22:20:37 +0300

    [Feature] Разбор заданий (#218)
    * [Feature] Разбор заданий
    
    * [Fix] Правка
    
    * Примеры рещения задач

---
+++ Study.Example/Tasks/Task5.cs
@@ -0,0 +1,37 @@
+﻿using Study.Example.Interfaces;
+
+namespace Study.Example.Tasks;
+
+public abstract class BaseClass2
+{
+    public virtual void VirtualMethod()
+    {
+        Console.WriteLine("Базовая реализация виртуального метода");
+    }
+
+    public abstract void AbstractMethod();
+}
+
+public class ChildrenClass2 : BaseClass2
+{
+    public override void AbstractMethod()
+    {
+        Console.WriteLine("Переопределенная реализация абстрактного метода");
+    }
+
+    public override void VirtualMethod()
+    {
+        Console.WriteLine("Переопределенная реализация виртуального метода");
+    }
+}
+
+public class Task5 : IRunnable
+{
+    public void Run()
+    {
+        var obj = new ChildrenClass2();
+
+        obj.AbstractMethod();
+        obj.VirtualMethod();
+    }
+}
