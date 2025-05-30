e94d035e09a0ec823c9b7c741c5763627b5c62fb
Александр Уколов <aggink@mail.ru>
2025-05-30 22:19:38 +0300

    Примеры рещения задач

---
+++ Study.Example/Tasks/Task2.cs
@@ -0,0 +1,29 @@
+﻿using Study.Example.Interfaces;
+using System.Reflection;
+
+namespace Study.Example.Tasks;
+
+public class MessagePrinter
+{
+    private void Print(string message)
+    {
+        Console.WriteLine($"Приватное сообщение: {message}");
+    }
+}
+
+public class Task2 : IRunnable
+{
+    public void Run()
+    {
+        var messagePrinter = new MessagePrinter();
+
+        var messagePrinterType = typeof(MessagePrinter);
+
+        var printMethodInfo = messagePrinterType.GetMethod("Print", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        if (printMethodInfo == null)
+            throw new Exception("Метод 'Print' не найден");
+
+        printMethodInfo.Invoke(messagePrinter, new object[] { "Привет" });
+    }
+}
