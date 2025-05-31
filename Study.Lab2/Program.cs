51d186f1ddbec868a637ae167ce08dd965e97675
chirique <onecherrypieplease@gmail.com>
2025-05-31 22:08:17 +0300

    [Feature] - добавлена лаба 2 (#214)
    * [Feature] - добавлена вторая лабораторная
    
    * [Feature] - добавлены тесты
    
    * [Fix] - вернула изначальные значения GROUP_NAME и PERSON_NUMBER
    
    * [Fix] - поправлено форматирование, добавлена папка dtomodels
    
    * [Fix] - убираю строковые json значения в юнит тестах

--- Study.Lab2/Program.cs
+++ Study.Lab2/Program.cs
@@ -15,6 +15,7 @@ using Study.Lab2.Logic.PresvyatoyKabachok;
 using Study.Lab2.Logic.Selestz;
 using Study.Lab2.Logic.xynthh;
 using Study.Lab2.Logic.gegemonTV;
+using Study.Lab2.Logic.chirique_online;
 
 public static class Program
 {
@@ -99,6 +100,8 @@ public static class Program
                 return new PresvyatoyKabachokService();
             case ("idb-23-03", 15):
                 return new gegemonTVService();
+            case ("idb-23-02", 12):
+                return new chiriqueOnlineService();
             default:
                 throw new NotSupportedException();
         }
