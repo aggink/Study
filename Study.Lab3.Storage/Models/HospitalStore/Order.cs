9211ba23e5b5c7b59f4a907c41db3594df0864b3
Jki748 <jki749@mail.ru>
2025-06-01 19:07:42 +0300

    [Private]

--- Study.Lab3.Storage/Models/HospitalStore/Order.cs
+++ Study.Lab3.Storage/Models/HospitalStore/Order.cs
@@ -7,25 +7,25 @@ namespace Study.Lab3.Storage.Models.HospitalStore;
 public class Order
 {
     /// <summary>
-    /// айди
+    /// Уникальный идентификатор заказа
     /// </summary>
     [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
     public Guid IsnOrder { get; set; }
 
     /// <summary>
-    /// айди пациента
+    /// Идентификатор пациента
     /// </summary>
     [Required]
     public Guid IsnPatient { get; set; }
 
     /// <summary>
-    /// айди товара
+    /// Идентификатор продукта
     /// </summary>
     [Required]
     public Guid IsnProduct { get; set; }
 
     /// <summary>
-    /// количество товара
+    /// Количество товара
     /// </summary>
     [Required, Range(ModelConstants.Order.QuantityMin, ModelConstants.Order.QuantityMax)]
     public int Quantity { get; set; }
