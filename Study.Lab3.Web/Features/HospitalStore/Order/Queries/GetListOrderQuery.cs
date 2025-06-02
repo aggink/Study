8d3fea1944be99beff2dbda2fbb2b1ae10250c06
Jki748 <94294136+Jki748@users.noreply.github.com>
2025-06-02 15:07:01 +0300

    [Feature] Добавлены модели для базы данных (#219)
    * [Feature] Добавлены модели для базы данных
    
    * [Feature] добавлены константы
    
    * [Feature] доработка моделей
    
    * [Feature] доработка моделей
    
    * [Fix] изменены using
    
    * [Private] изменены комменарии
    
    * [Feature] добавлен DataContext
    
    * [Feature] добавлены миграции
    
    * [Feature] добавлена работа с Order
    
    * [Feature] добавлена работа с Patient и Product
    
    * [Feature] добавлены регистрации сервисов
    
    * [Private]
    
    * [Private]
    
    * [Private] попытка исправить кодировку
    
    * [Private] правка кодировки

---
+++ Study.Lab3.Web/Features/HospitalStore/Order/Queries/GetListOrderQuery.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Study.Lab3.Storage.Database;
+using Study.Lab3.Web.Features.HospitalStore.Order.DtoModels;
+
+namespace Study.Lab3.Web.Features.HospitalStore.Order.Queries;
+
+/// <summary>
+/// Получение списка заказов
+/// </summary>
+public sealed class GetListOrderQuery : IRequest<OrderDto[]>
+{
+}
+
+public sealed class GetListOrderQueryHandler : IRequestHandler<GetListOrderQuery, OrderDto[]>
+{
+    private readonly DataContext _dataContext;
+
+    public GetListOrderQueryHandler(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    public async Task<OrderDto[]> Handle(GetListOrderQuery request, CancellationToken cancellationToken)
+    {
+        return await _dataContext.Orders
+            .AsNoTracking()
+            .Select(x => new OrderDto
+            {
+                IsnOrder = x.IsnOrder,
+                IsnPatient = x.IsnPatient,
+                IsnProduct = x.IsnProduct,
+                Quantity = x.Quantity
+            })
+            .OrderByDescending(x => x.IsnOrder) // или .OrderByDescending(x => x.CreatedAt), если есть
+            .ToArrayAsync(cancellationToken);
+    }
+}
