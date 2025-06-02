fef468119f5d5b6d7a329c42660c587f2af9f872
Jki748 <94294136+Jki748@users.noreply.github.com>
2025-06-02 15:07:01 +0300

    [Feature] Добавлены модели для базы данных (#219)
    * [Feature] Добавлены модели для базы данных
    
    * [Feature] добавлены константы
    
    * [Feature] доработка моделей
    
    * [Feature] доработка моделей
    
    * [Fix] изменены using
    
    * [Private] изменены комменарии
    
    * [Feature] добавлен DataContext
    
    * [Feature] добавлены миграции
    
    * [Feature] добавлена работа с Order
    
    * [Feature] добавлена работа с Patient и Product
    
    * [Feature] добавлены регистрации сервисов
    
    * [Private]
    
    * [Private]
    
    * [Private] попытка исправить кодировку
    
    * [Private] правка кодировки

---
+++ Study.Lab3.Web/Features/HospitalStore/Patient/DtoModels/PatientWithHistoryDto.cs
@@ -0,0 +1,34 @@
+﻿namespace Study.Lab3.Web.Features.HospitalStore.Patient.DtoModels;
+
+public sealed record PatientWithDetailsDto
+{
+    /// <summary>
+    /// Идентификатор пациента
+    /// </summary>
+    public Guid IsnPatient { get; init; }
+
+    /// <summary>
+    /// ФИО пациента
+    /// </summary>
+    public string FullName { get; init; }
+
+    /// <summary>
+    /// Номер медицинской карты
+    /// </summary>
+    public string MedicalCardId { get; init; }
+
+    /// <summary>
+    /// Контактный телефон
+    /// </summary>
+    public string Phone { get; init; }
+
+    /// <summary>
+    /// Количество заказов пациента
+    /// </summary>
+    public int TotalOrders { get; init; }
+
+    /// <summary>
+    /// Дата последнего заказа
+    /// </summary>
+    public DateTime? LastOrderDate { get; init; }
+}
