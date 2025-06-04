46bd78244e813fcf254e27afbb03547fb11b93b5
alkeivi <k3ndal-l@yandex.ru>
2025-06-04 16:02:52 +0300

    [Feature] ЛР3. Добавлены модели, логика и интерфейсы (#225)
    * [Feature] ЛР3. Добавлены модели, логика и интерфейсы
    
    * [Fix] Исправлены ключи и названия моделей
    
    * [Feature] Добавлены миграции
    
    * [Fix] Переименованы файлы
    
    * [Fix] Изменены названия
    
    * [Feature] Добавлены миграции
    
    * [Feature] Добавлен раздел Web: controllers, dtomodels, commands, queries
    
    * [Fix] Пересозданы миграции
    
    * [Fix] Исправлена логика и DtoModels
    
    * [Fix] Исправлены модели и пересозданы миграции
    
    * [Fix] Обновлены миграции
    
    * [Fix] Откат миграций и исправление BeautyClient
    
    * [Fix] Пересозданы миграции
    
    * [Fix] Исправлены связи между таблицами
    
    * [Fix] Исправлены ошибки
    
    * [Fix] Пересозданы миграции
    
    * [Fix] Исправлены ошибки
    
    * [Fix] Пересозданы миграции
    
    * [Fix] Исправлены ошибки и добавлены пробельные строки
    
    * [Fix] Пересозданы миграции
    
    * [Fix] Исправление ошибок
    
    * [Fix] Пересозданы миграции

--- Study.Lab3.Storage/Database/DataContext.cs
+++ Study.Lab3.Storage/Database/DataContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Study.Lab3.Storage.Models.BeautySalon;
 using Study.Lab3.Storage.Models.Cinema;
+using Study.Lab3.Storage.Models.HospitalStore;
 using Study.Lab3.Storage.Models.Library;
 using Study.Lab3.Storage.Models.Restaurants;
 using Study.Lab3.Storage.Models.University;
-using Study.Lab3.Storage.Models.HospitalStore;
 
 namespace Study.Lab3.Storage.Database;
 
@@ -206,6 +207,25 @@ public class DataContext : DbContext
     /// Позиции заказов
     /// </summary>
     public virtual DbSet<OrderItem> OrderItems { get; set; }
+
+    #endregion
+
+    #region BeautySalon
+
+    /// <summary>
+    /// Клиенты
+    /// </summary>
+    public virtual DbSet<BeautyClient> BeautyClient { get; set; }
+
+    /// <summary>
+    /// Услуги
+    /// </summary>
+    public virtual DbSet<BeautyService> BeautyService { get; set; }
+
+    /// <summary>
+    /// Записи на услуги
+    /// </summary>
+    public virtual DbSet<BeautyAppointment> BeautyAppointment { get; set; }
 
     #endregion
 }
