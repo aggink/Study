fb98d24314752488f16e3ba51feda0b10fb9d175
Alex <64156241+neijrr@users.noreply.github.com>
2025-06-06 01:19:49 +0300

    [Feature] Добавлена ЛР 2 (#241)
    * [Feature] Добавлена ЛР 2
    
    * [Fix] Добавил пропавшие using в Program.cs
    
    * [Feature] Add lab 3 (#234)
    
    * [Feature] init 3 lab
    
    * resolve conflicts
    
    * Add models
    
    * [Feature] Add lab 3
    
    * [Fix] Conflicts resolved, migrations recreated
    
    * [Fix] Style fix
    
    * [Fix] recreate migrations
    
    * [Fix] Id -> Isn
    
    * [Fix] set -> init
    
    * [Fix] исправил недочеты
    
    * [Fix] Убраны лишние свойства CatDto
    
    * [Fix] Merge + recreate migrations
    
    * [Fix] delete migrations
    
    * [Fix] Recreate migrations
    
    * [Feature] Лаба 3 (#247)
    
    * [Feature]
    
    * [Fix]
    
    * [Fix]
    
    * [Private] Исправления формата
    
    ---------
    
    Co-authored-by: lsoko1l14l <Misasokolov76@gmail.com>
    Co-authored-by: chaspix <106628829+chaspix@users.noreply.github.com>

---
+++ Study.Lab2.Logic.Interfaces/neijrr/IUrlBuilder.cs
@@ -0,0 +1,7 @@
+namespace Study.Lab2.Logic.Interfaces.neijrr;
+
+public interface IUrlBuilder
+{
+    string BaseUrl { get; set; }
+    string Url(List<object> path, string protocol = null, Dictionary<object, object> parameters = null);
+}
--- Study.Lab2/Program.cs
+++ Study.Lab2/Program.cs
@@ -1,5 +1,6 @@
-﻿using Study.Lab2.Logic.alkeivi;
-using Study.Lab2.Logic.Assistant;
+﻿using Study.Lab2.Logic.Assistant;
+using Study.Lab2.Logic.Interfaces;
+using Study.Lab2.Logic.alkeivi;
 using Study.Lab2.Logic.brnvika;
 using Study.Lab2.Logic.Cherryy;
 using Study.Lab2.Logic.chirique_online;
@@ -7,7 +8,6 @@ using Study.Lab2.Logic.danaky1;
 using Study.Lab2.Logic.eldarovskiy;
 using Study.Lab2.Logic.freaxd;
 using Study.Lab2.Logic.gegemonTV;
-using Study.Lab2.Logic.Interfaces;
 using Study.Lab2.Logic.Jki749;
 using Study.Lab2.Logic.katty;
 using Study.Lab2.Logic.kinkiss1;
@@ -15,16 +15,13 @@ using Study.Lab2.Logic.KirillPoroshin;
 using Study.Lab2.Logic.love100rubb;
 using Study.Lab2.Logic.lsokol14l;
 using Study.Lab2.Logic.mariabyrrrrak;
+using Study.Lab2.Logic.neijrr;
 using Study.Lab2.Logic.p0se1d0n;
 using Study.Lab2.Logic.poigko;
 using Study.Lab2.Logic.PresvyatoyKabachok;
 using Study.Lab2.Logic.Pro100futa;
 using Study.Lab2.Logic.Selestz;
-using Study.Lab2.Logic.xynthh;
-using Study.Lab2.Logic.KirillPoroshin;
 using Study.Lab2.Logic.Taipano;
-using Study.Lab2.Logic.gegemonTV;
-using Study.Lab2.Logic.chirique_online;
 using Study.Lab2.Logic.TucKaW;
 using Study.Lab2.Logic.xynthh;
 
@@ -127,6 +124,8 @@ public static class Program
                 return new danaky1Service();
             case ("idb-23-03", 16):
                 return new freaxdService();
+            case ("idb-23-03", 6):
+                return new neijrrService();
             default:
                 throw new NotSupportedException();
         }
