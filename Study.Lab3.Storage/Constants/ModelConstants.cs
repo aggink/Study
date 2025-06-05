08c1691d98732c34c0c2838224ceef4d01d43f5b
lsoko1l14l <Misasokolov76@gmail.com>
2025-06-05 05:10:56 +0300

    [Feature] Add lab 3 (#234)
    * [Feature] init 3 lab
    
    * resolve conflicts
    
    * Add models
    
    * [Feature] Add lab 3
    
    * [Fix] Conflicts resolved, migrations recreated
    
    * [Fix] Style fix
    
    * [Fix] recreate migrations
    
    * [Fix] Id -> Isn
    
    * [Fix] set -> init
    
    * [Fix] исправил недочеты
    
    * [Fix] Убраны лишние свойства CatDto
    
    * [Fix] Merge + recreate migrations
    
    * [Fix] delete migrations
    
    * [Fix] Recreate migrations

--- Study.Lab3.Storage/Constants/ModelConstants.cs
+++ Study.Lab3.Storage/Constants/ModelConstants.cs
@@ -334,6 +334,88 @@ public static class ModelConstants
         public const int MaxParticipantValue = 100;
     }
 
+
+    public static class Cat
+    {
+        /// <summary>
+        /// Максимальная длина клички кота
+        /// </summary>
+        public const int Nickname = 100;
+
+        /// <summary>
+        /// Максимальная длина описания кота
+        /// </summary>
+        public const int Description = 500;
+
+        /// <summary>
+        /// Максимальная длина названия породы
+        /// </summary>
+        public const int Breed = 100;
+
+        /// <summary>
+        /// Максимальная длина названия окраса
+        /// </summary>
+        public const int Color = 20;
+
+        /// <summary>
+        /// Максимальная длина истории болезней
+        /// </summary>
+        public const int MedicalHistory = 1000;
+
+        /// <summary>
+        /// Минимальный возраст кота (в годах)
+        /// </summary>
+        public const int AgeMin = 0;
+
+        /// <summary>
+        /// Максимальный возраст кота (в годах)
+        /// </summary>
+        public const int AgeMax = 30;
+
+        /// <summary>
+        /// Минимальный вес кота (в кг)
+        /// </summary>
+        public const double WeightMin = 0.5;
+
+        /// <summary>
+        /// Максимальный вес кота (в кг)
+        /// </summary>
+        public const double WeightMax = 15.0;
+        
+        /// <summary>
+        /// Ссылка на фотографию кота
+        /// </summary>
+        public const int PhotoUrl = 100;
+    }
+
+    public static class ShelterCustomer
+    {
+        /// <summary>
+        /// Максимальная длина имени клиента
+        /// </summary>
+        public const int Name = 100;
+
+        /// <summary>
+        /// Максимальная длина описания клиента
+        /// </summary>
+        public const int Description = 500;
+
+        /// <summary>
+        /// Максимальная длина адреса клиента
+        /// </summary>
+        public const int Address = 100;
+
+        /// <summary>
+        /// Максимальная длина номера телефона клиента
+        /// </summary>
+        public const int PhoneNumber = 15;
+
+        /// <summary>
+        /// Максимальная длина email клиента
+        /// </summary>
+        public const int Email = 255;
+    }
+
     public static class Session
     {
         /// <summary>
@@ -468,6 +550,24 @@ public static class ModelConstants
         /// Максимально допустимое количество участников.
         /// </summary>
         public const int MaxPart = 10;
+    }
+
+    public static class Adoption
+    {
+        /// <summary>
+        /// Минимальная цена за усыновление
+        /// </summary>
+        public const int PriceMin = 0;
+
+        /// <summary>
+        /// Максимальная цена за усыновление
+        /// </summary>
+        public const int PriceMax = 1_000_000;
+
+        /// <summary>
+        /// Максимальная длина статуса усыновления
+        /// </summary>
+        public const int Status = 50;
     }
     public static class Restaurant
     {
---
+++ Study.Lab3.Web/Features/Shelter/Cats/Queries/GetCatByIdQuery.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using CoreLib.Common.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Study.Lab3.Storage.Database;
+using Study.Lab3.Web.Features.Shelter.Cats.DtoModels;
+
+namespace Study.Lab3.Web.Features.Shelter.Cats.Queries;
+
+/// <summary>
+/// Получение кота по идентификатору
+/// </summary>
+public sealed class GetCatByIdQuery : IRequest<CatDto>
+{
+    /// <summary>
+    /// Идентификатор кота
+    /// </summary>
+    [Required]
+    [FromQuery]
+    public Guid IsnCat { get; init; }
+}
+
+public sealed class GetCatByIdQueryHandler : IRequestHandler<GetCatByIdQuery, CatDto>
+{
+    private readonly DataContext _dataContext;
+
+    public GetCatByIdQueryHandler(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    public async Task<CatDto> Handle(GetCatByIdQuery request, CancellationToken cancellationToken)
+    {
+        var cat = await _dataContext.Cats
+                      .AsNoTracking()
+                      .FirstOrDefaultAsync(c => c.IsnCat == request.IsnCat, cancellationToken)
+                  ?? throw new BusinessLogicException(
+                      $"Кот с идентификатором \"{request.IsnCat}\" не существует");
+
+        return new CatDto
+        {
+            IsnCat = cat.IsnCat,
+            Nickname = cat.Nickname,
+            BirthDate = cat.BirthDate,
+            Description = cat.Description,
+            Breed = cat.Breed,
+            IsVaccinated = cat.IsVaccinated,
+            IsSterilized = cat.IsSterilized,
+            Color = cat.Color,
+            MedicalHistory = cat.MedicalHistory,
+            PhotoUrl = cat.PhotoUrl,
+            ArrivalDate = cat.ArrivalDate,
+            IsAvailableForAdoption = cat.IsAvailableForAdoption,
+            Age = cat.Age,
+            Weight = cat.Weight
+        };
+    }
+}
