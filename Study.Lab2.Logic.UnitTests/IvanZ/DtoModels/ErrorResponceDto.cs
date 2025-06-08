203c1b4b399fd83e889045fa9e5f94291c68369c
IvanZ-code <114724416+IvanZ-code@users.noreply.github.com>
2025-06-08 13:35:32 +0300

    Update ErrorResponceDto.cs

--- Study.Lab2.Logic.UnitTests/IvanZ/DtoModels/ErrorResponceDto.cs
+++ Study.Lab2.Logic.UnitTests/IvanZ/DtoModels/ErrorResponceDto.cs
@@ -1,1 +1,12 @@
+using System.Text.Json.Serialization;
 
+namespace Study.Lab2.Logic.UnitTests.alkeivi.DtoModels;
+
+public sealed record ErrorResponseDto
+{
+    [JsonPropertyName("message")]
+    public string Message { get; init; }
+
+    [JsonPropertyName("statusCode")]
+    public int StatusCode { get; init; }
+}
