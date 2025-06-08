fb40262c5342423b8247a91120f02cd84c107344
IvanZ-code <114724416+IvanZ-code@users.noreply.github.com>
2025-06-08 13:35:55 +0300

    Update ResponceDto.cs

--- Study.Lab2.Logic.UnitTests/IvanZ/DtoModels/ResponceDto.cs
+++ Study.Lab2.Logic.UnitTests/IvanZ/DtoModels/ResponceDto.cs
@@ -1,1 +1,9 @@
+﻿using System.Text.Json.Serialization;
 
+namespace Study.Lab2.Logic.UnitTests.alkeivi.DtoModels;
+
+public sealed record ResponseDto
+{
+    [JsonPropertyName("message")]
+    public string Message { get; init; }
+}
