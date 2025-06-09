5aea416936ad0374daa0e8c73dffff5069cd3351
Maxtir23 <longtomas53@gmail.com>
2025-06-09 19:04:22 +0300

    [Feature] лаба 1, задание 1. (#272)
    * [Feature] лаба 1, задание 1.
    
    * [Fix] внесены правки

---
+++ Study.Lab2.Logic/Maxtir23/Maxtir23Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.Lab2.Logic.Maxtir23
+{
+    internal class Maxtir23Service
+    {
+    }
+}
