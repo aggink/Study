2cff0febd36f840ea8dc27d58aa4f1e081ef6610
Maxtir23 <longtomas53@gmail.com>
2025-06-09 20:09:27 +0300

    [Feature] добавлена лаба 2

--- Study.Lab2.Logic/Maxtir23/RequestService.cs
+++ Study.Lab2.Logic/Maxtir23/RequestService.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Study.Lab2.Logic.Maxtir23;
+using Study.Lab2.Logic.Interfaces.Maxtir23;
 
-namespace Study.Lab2.Logic.Maxtir23
+namespace Study.Lab2.Logic.Maxtir23;
+
+public class RequestService : IRequestService
 {
-    internal class RequestService
+    private readonly HttpClient _httpClient;
+
+    public RequestService(HttpClient httpClient)
     {
+        _httpClient = httpClient;
+    }
+
+    public string FetchData(string url)
+    {
+        var response = _httpClient.GetAsync(url).GetAwaiter().GetResult();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Mistake: {response.StatusCode} - {response.ReasonPhrase} !!!");
+        }
+
+        using (var stream = response.Content.ReadAsStream())
+        using (var reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+
+    public async Task<string> FetchDataAsync(string url, CancellationToken cancellationToken)
+    {
+        var response = await _httpClient.GetAsync(url, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Mistake: {response.StatusCode} - {response.ReasonPhrase} !!!");
+        }
+
+        return await response.Content.ReadAsStringAsync(cancellationToken);
+    }
+
+    public void Dispose()
+    {
+        _httpClient.Dispose();
     }
 }
