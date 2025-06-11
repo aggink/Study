8a587d7d638c27d261b130ed65793c607e8f4b0e
Kotsutaki <konstantin2005.post@gmail.com>
2025-06-11 22:41:04 +0300

    fix

--- Study.Lab2/Program.cs
+++ Study.Lab2/Program.cs
@@ -38,12 +38,12 @@ public static class Program
     /// <summary>
     /// Название группы
     /// </summary>
-    private const string GROUP_NAME = "idb-23-02";
+    private const string GROUP_NAME = "assistant";
 
     /// <summary>
     /// Порядковый номер
     /// </summary>
-    private const int PERSON_NUMBER = 9;
+    private const int PERSON_NUMBER = 1;
 
     public static async Task Main()
     {
