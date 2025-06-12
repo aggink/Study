b4e37c8a05cf4a8315d4bd83ee11dbf95e3f676c
Crocodile17 <for.vuz.alexr@gmail.com>
2025-06-12 14:00:35 +0300

    [fix] исправил лабу 3

---
+++ Study.Lab3.Storage.PostgreSQL/Migrations/20250612105247_PingpongClub.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Study.Lab3.Storage.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class PingpongClub : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pingpongclub",
+                columns: table => new
+                {
+                    IsnPingpongclub = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsnStudent = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsnSubject = table.Column<Guid>(type: "uuid", nullable: false),
+                    ParticipantsCount = table.Column<int>(type: "integer", nullable: false),
+                    PingpongclubDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pingpongclub", x => x.IsnPingpongclub);
+                    table.ForeignKey(
+                        name: "FK_Pingpongclub_Students_IsnStudent",
+                        column: x => x.IsnStudent,
+                        principalTable: "Students",
+                        principalColumn: "IsnStudent",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Pingpongclub_Subjects_IsnSubject",
+                        column: x => x.IsnSubject,
+                        principalTable: "Subjects",
+                        principalColumn: "IsnSubject",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pingpongclub_IsnStudent",
+                table: "Pingpongclub",
+                column: "IsnStudent");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pingpongclub_IsnSubject",
+                table: "Pingpongclub",
+                column: "IsnSubject");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pingpongclub");
+        }
+    }
+}
