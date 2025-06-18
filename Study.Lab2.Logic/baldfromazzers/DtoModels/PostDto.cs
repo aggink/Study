eb1406297f5d1b3fc5073ac2f6d36870214c2da9
baldfromazzers <maksmaj79@gmail.com>
2025-06-19 02:45:38 +0300

    addlab2

---
+++ Study.Lab2.Logic/baldfromazzers/DtoModels/PostDto.cs
@@ -0,0 +1,19 @@
+using System.Text.Json.Serialization;
+
+namespace Study.Lab2.Logic.baldfromazzers.DtoModels;
+
+public class PostDto
+{
+    [JsonPropertyName("userId")]
+    public int UserId { get; set; }
+    
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    
+    [JsonPropertyName("body")]
+    public string Body { get; set; }
+
+}
---
+++ Study.Lab2.Logic/baldfromazzers/DtoModels/UserDto.cs
@@ -0,0 +1,19 @@
+using System.Text.Json.Serialization;
+
+namespace Study.Lab2.Logic.baldfromazzers.DtoModels;
+
+public class UserDto
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    
+    [JsonPropertyName("username")]
+    public string Username { get; set; }
+    
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+
+}
