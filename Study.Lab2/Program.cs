128370b8f8a5e594d9b38cce4b98a7fcf9020aef
fableslots <evgeniymoiseev2005@mail.ru>
2025-06-19 01:06:48 +0300

    [Feature]Добавил ЛР2

--- Study.Lab2/Program.cs
+++ Study.Lab2/Program.cs
@@ -31,6 +31,7 @@ using Study.Lab2.Logic.yamisakimei;
 using Study.Lab2.Logic.SuperSalad007;
 using Study.Lab2.Logic.Bonnemort;
 using Study.Lab2.Logic.eduardvafin56;
+using Study.Lab2.Logic.fableslots;
 
 public static class Program
 {
@@ -149,6 +150,8 @@ public static class Program
                 return new BonnemortService();
             case ("idb-23-02", 7):
                 return new Eduardvafin56Service();
+            case ("idb-23-02", 14):
+                return new fableslotsService();
             default:
                 throw new NotSupportedException();
         }
