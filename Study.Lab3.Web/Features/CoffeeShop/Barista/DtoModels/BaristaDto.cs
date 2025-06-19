38e3805973e83a908f960587d64e5087b735b033
baldfromazzers <maksmaj79@gmail.com>
2025-06-19 13:20:28 +0300

    fix

--- Study.Lab3.Web/Features/CoffeeShop/Barista/DtoModels/BaristaDto.cs
+++ Study.Lab3.Web/Features/CoffeeShop/Barista/DtoModels/BaristaDto.cs
@@ -3,55 +3,55 @@ namespace Study.Lab3.Web.Features.CoffeeShop.Barista.DtoModels;
 /// <summary>
 /// DTO бариста
 /// </summary>
-public class BaristaDto
+sealed record BaristaDto
 {
     /// <summary>
     /// Идентификатор бариста
     /// </summary>
-    public Guid IsnBarista { get; set; }
+    public Guid IsnBarista { get; init; }
 
     /// <summary>
     /// Имя бариста
     /// </summary>
-    public string FirstName { get; set; }
+    public string FirstName { get; init; }
 
     /// <summary>
     /// Фамилия бариста
     /// </summary>
-    public string LastName { get; set; }
+    public string LastName { get; init; }
 
     /// <summary>
     /// Телефон бариста
     /// </summary>
-    public string Phone { get; set; }
+    public string Phone { get; init; }
 
     /// <summary>
     /// Email бариста
     /// </summary>
-    public string Email { get; set; }
+    public string Email { get; init; }
 
     /// <summary>
     /// Опыт работы в годах
     /// </summary>
-    public int Experience { get; set; }
+    public int Experience { get; init; }
 
     /// <summary>
     /// Специализация бариста
     /// </summary>
-    public string Specialization { get; set; }
+    public string Specialization { get; init; }
 
     /// <summary>
     /// Зарплата в месяц
     /// </summary>
-    public double? Salary { get; set; }
+    public double? Salary { get; init; }
 
     /// <summary>
     /// Дата найма
     /// </summary>
-    public DateTime HireDate { get; set; }
+    public DateTime HireDate { get; init; }
 
     /// <summary>
     /// Активен ли сотрудник
     /// </summary>
-    public bool IsActive { get; set; }
+    public bool IsActive { get; init; }
 }
